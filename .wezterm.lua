4f14a6ff7304c417c8af7accd7d5149d4b78338b
Piyush <pradhanpiyush687@gmail.com>
2024-03-01 02:25:33 -0500

    Cleanup

--- .wezterm.lua
+++
@@ -1,6 +0,0 @@
-local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
-config.font = wezterm.font 'Zed Mono'
-config.font_size = 12
-config.hide_tab_bar_if_only_one_tab = true
-return config
--- setup_config.sh
+++
@@ -1,8 +0,0 @@
-if [ "$(uname -s)" = "Linux" ]; then
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-fi
-
-cp -r nvim/ ~/.config
-cp -r alacritty/ ~/.config
-cp .tmux.conf ~/
