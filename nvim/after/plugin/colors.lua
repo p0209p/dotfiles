0ffda794425341a1b5243d0a6e2e25f764fefd49
Piyush Pradhan <pradhan@Piyushs-Laptop.local>
2024-04-09 02:26:18 -0400

    Rose pine is the best

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,17 +1,20 @@
-require('poimandres').setup({
-  bold_vert_split = false,
-  dim_nc_background = false,
-  disable_background = false,
-  disable_float_background = false,
-  disable_italics = true,
+require("rose-pine").setup({
+    variant = "main",
+    dark_variant = "main",
+    dim_inactive_windows = false,
+    extend_background_behind_borders = true,
+
+    enable = {
+        terminal = true,
+        legacy_highlights = true,
+        migrations = true,
+    },
+
+    styles = {
+        bold = true,
+        italic = false,
+        transparency = true,
+    },
 })
 
-vim.cmd.colorscheme("poimandres")
-
--- vim.cmd("highlight Normal guibg=NONE")
--- vim.cmd("highlight LineNr guibg=NONE guifg=#80848c")
--- vim.cmd("highlight WinSeparator guibg=NONE")
--- vim.cmd("highlight SignColumn guibg=NONE")
--- vim.cmd("highlight VertSplit guibg=NONE")
--- vim.cmd("highlight StatusLine guibg=NONE guifg=#80848c")
--- vim.cmd("highlight StatusLineNC guibg=NONE guifg=#80848c")
+vim.cmd.colorscheme("rose-pine")
