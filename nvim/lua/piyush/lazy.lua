e1cbb74df969f81387c9e28cc7387b8d654888b9
piyush-ppradhan <pradhanpiyush687@gmail.com>
2024-05-04 03:59:15 -0400

    Latest config

--- nvim/lua/piyush/lazy.lua
+++ nvim/lua/piyush/lazy.lua
@@ -177,7 +177,7 @@ require("lazy").setup({
 
 	{ "nvim-tree/nvim-web-devicons", lazy = false },
 	{ "JuliaEditorSupport/julia-vim", lazy = false },
-	{ "rose-pine/neovim", lazy = false},
+	{ "rose-pine/neovim", lazy = false },
 
 	-- Linter and Formatter
 	{ "stevearc/conform.nvim", lazy = false, event = { "BufReadPre", "BufNewFile" } },
